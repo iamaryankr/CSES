d58dd43d6b251a1b68ef623945762ee1189dc6ab
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2024-08-07 03:38:57 +0530

    maths

---
+++ maths/exponentiation.cpp
@@ -0,0 +1,74 @@
+//iamaryankr
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define pob pop_back
+#define all(x) (x).begin(), (x).end()
+#define debugV(v) for(int i=0;i<v.size();i++) cout<<v[i]<<' '; cout<<endl;
+#define debug(x) cout<< #x << " = " << x << endl;
+#define debugMat(v) for(int i=0; i<v.size(); i++){ for(int j=0; j<v[0].size(); j++){ cout << v[i][j] << " ";} cout << nl;}
+#define nl "\n"
+#define ff first;
+#define ss second;
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+ 
+typedef long double ld;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef map<int,int> mii;
+ 
+int gcd(int a, int b){ return (b ? gcd(b, a % b) : a); }
+int lcm(int a, int b){ return (a*b/gcd(a,b)); }
+ 
+int delrow[] = {-1, 0, 1, 0};
+int delcol[] = {0, 1, 0, -1};
+bool valid(int i, int j, int n, int m) {return i>=0 && i<n && j<m && j>=0;}
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+const ll INFF = 1e18;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//try thinking Binary search(on ans), Bit manipulation, Dp
+//jee standard Maths, greedy, Bfs, Dfs, 2 pointers, bitmask
+//sliding window
+
+//computes a^(b) MODULO M
+ll binexp(ll a, ll b){
+    ll ans = 1;
+    while(b>0){
+        if(b&1)
+            ans = ans*a%MOD;
+        a = (a*a)%MOD;
+        b = (b/2);
+    }
+    return ans%MOD;
+}
+
+void solve(){
+    ll a, b;
+    cin >> a >> b;
+    cout << binexp(a, b) << nl;
+}
+   
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // cout << setprecision(12) << fixed;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ maths/sumofDivisors.cpp
@@ -0,0 +1,89 @@
+//iamaryankr
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define pob pop_back
+#define all(x) (x).begin(), (x).end()
+#define debugV(v) cout<<#v<<" = [ "; for(int i=0;i<v.size();i++) cout<<v[i]<<' '; cout<<"]"<<nl;
+#define debug(x) cout<< #x << " = " << x << endl;
+#define debugMat(v) for(int i=0; i<v.size(); i++){ for(int j=0; j<v[0].size(); j++){ cout << v[i][j] << " ";} cout << nl;}
+#define nl "\n"
+#define ff first;
+#define ss second;
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+ 
+typedef long double ld;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef map<int,int> mii;
+ 
+int gcd(int a, int b){ return (b ? gcd(b, a % b) : a); }
+int lcm(int a, int b){ return (a*b/gcd(a,b)); }
+ 
+int delrow[] = {-1, 0, 1, 0};
+int delcol[] = {0, 1, 0, -1};
+bool valid(int i, int j, int n, int m) {return i>=0 && i<n && j<m && j>=0;}
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+const ll INFF = 1e18;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//try thinking Binary search(on ans), Bit manipulation, Dp
+//jee standard Maths, greedy, Bfs, Dfs, 2 pointers, bitmask
+//sliding window
+
+
+//counting the sum of all the divisors
+// sum of GP
+
+
+ll inverse(ll i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+
+void solve(){
+    ll n;
+    cin >> n; 
+    ll ans=0;
+    for(ll i=1;i*i<=n;i++){
+        ans+=((n/i)*i)%MOD;
+        ans%=MOD;
+    }
+    ll l=(ll)sqrt(n);
+    for(ll i=sqrt(n);i>=1;i--){
+        ll r=n/i;
+        ll sum=0;
+        sum+=((((r%MOD)*((r+1)%MOD))%MOD)*inverse(2))%MOD;
+        sum%=MOD;
+        sum-=((((l%MOD)*((l+1)%MOD))%MOD)*inverse(2))%MOD;
+        sum=(sum+MOD)%MOD;
+        sum=(sum*i)%MOD;
+        l=r;
+        ans=(ans+sum)%MOD;
+    }
+    cout<<ans;
+}
+   
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // cout << setprecision(12) << fixed;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
